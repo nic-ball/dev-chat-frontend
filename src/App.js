4bcd17bede3831679fe0de1fa07c96e636ccc827
nic-ball <nicholasball147@gmail.com>
2020-11-14 14:55:22 +0000

    Initial setup and basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import CreateTeam from "./components/team/CreateTeam";
+import CreateMessage from "./components/message/CreateMessage";
+import CreateChannel from "./components/channel/CreateChannel";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/create-team" component={CreateTeam} />
+          <Route path="/create-message" component={CreateMessage} />
+          <Route path="/create-channel" component={CreateChannel} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Auth or Slack</div>;
+};
+
+export default Home;
